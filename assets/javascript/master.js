fa4732490f08fecb6724a1a0e7f6ee6d16942b36
Nageshwar Tripathi <stpandstp.martialstylus@gmail.com>
2023-11-28 23:34:00 +0530

    completed:navbar-com

---
+++ assets/javascript/master.js
@@ -0,0 +1,6 @@
+document.getElementById("nav-open").addEventListener("click", function () {
+    document.getElementById("nav-menu").style = "display: flex;";
+});
+document.getElementById("nav-close").addEventListener("click", function () {
+    document.getElementById("nav-menu").style = "display: none;";
+});
